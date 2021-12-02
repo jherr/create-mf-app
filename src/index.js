753cc12abb43f9f3653b0d8b1df4c1ac9cb44924
Jack Herrington <jherr@pobox.com>
2021-12-01 20:05:47 -0800

    Trying to fix Windows

--- src/index.js
+++ src/index.js
@@ -1,26 +1,29 @@
-const util = require('util')
-const fs = require('fs')
-const path = require('path')
-const glob = require('glob')
+const util = require("util");
+const fs = require("fs");
+const path = require("path");
+const glob = require("glob");
 
-const ncp = util.promisify(require('ncp').ncp)
+const ncp = util.promisify(require("ncp").ncp);
 
 const templateFile = (fileName, replacements) => {
-  let contents = fs.readFileSync(fileName, 'utf8').toString()
+  let contents = fs.readFileSync(fileName, "utf8").toString();
   Object.keys(replacements).forEach((key) => {
     contents = contents.replace(
-      new RegExp(`(\{\{${key}\}\}|\{\{ ${key} \}\})`, 'g'), //eslint-disable-line
+      new RegExp(`(\{\{${key}\}\}|\{\{ ${key} \}\})`, "g"), //eslint-disable-line
       replacements[key]
-    )
-  })
-  fs.writeFileSync(fileName, contents)
-}
+    );
+  });
+  fs.writeFileSync(fileName, contents);
+};
 
 // required for npm publish
 const renameGitignore = (projectName) => {
   const projectPath = path.join(__dirname, `../${projectName}`);
-  if (fs.existsSync(`${projectPath}/gitignore`)) {
-    fs.renameSync(`${projectPath}/gitignore`, `${projectPath}/.gitignore`);
+  if (fs.existsSync(path.normalize(`${projectPath}/gitignore`))) {
+    fs.renameSync(
+      path.normalize(`${projectPath}/gitignore`),
+      path.normalize(`${projectPath}/.gitignore`)
+    );
   }
 };
 
@@ -33,72 +36,72 @@ const renameGitignore = (projectName) => {
 //   - port: Port to run the project on
 
 module.exports = async ({ type, language, framework, name, css, port }) => {
-  const lang = language === 'typescript' ? 'ts' : 'js'
+  const lang = language === "typescript" ? "ts" : "js";
 
   const replacements = {
     NAME: name,
     FRAMEWORK: framework,
-    SAFE_NAME: name.replace(/-/g, '_').trim(),
-    LANGUAGE: language === 'typescript' ? 'TypeScript' : 'JavaScript'
+    SAFE_NAME: name.replace(/-/g, "_").trim(),
+    LANGUAGE: language === "typescript" ? "TypeScript" : "JavaScript",
+  };
+
+  const tempDir = type.toLowerCase();
+
+  if (type === "Library") {
+    await ncp(path.join(__dirname, `../templates/${tempDir}/typescript`), name);
   }
 
-  const tempDir = type.toLowerCase()
-
-  if (type === 'Library') {
-    await ncp(path.join(__dirname, `../templates/${tempDir}/typescript`), name)
-  }
-
-  if (type === 'Server') {
-    replacements.PORT = port
+  if (type === "Server") {
+    replacements.PORT = port;
 
     await ncp(
       path.join(__dirname, `../templates/${tempDir}/${framework}`),
       name
-    )
+    );
   }
 
-  if (type === 'Application') {
+  if (type === "Application") {
     await ncp(
       path.join(__dirname, `../templates/${tempDir}/${framework}/base`),
       name
-    )
+    );
     await ncp(
       path.join(__dirname, `../templates/${tempDir}/${framework}/${lang}`),
       name
-    )
+    );
 
-    const tailwind = css === 'Tailwind'
-    replacements.CSS_EXTENSION = tailwind ? 'scss' : 'css'
+    const tailwind = css === "Tailwind";
+    replacements.CSS_EXTENSION = tailwind ? "scss" : "css";
     replacements.CONTAINER = tailwind
-      ? 'mt-10 text-3xl mx-auto max-w-6xl'
-      : 'container'
-    replacements.CSS = tailwind ? 'Tailwind' : 'Empty CSS'
-    replacements.PORT = port
+      ? "mt-10 text-3xl mx-auto max-w-6xl"
+      : "container";
+    replacements.CSS = tailwind ? "Tailwind" : "Empty CSS";
+    replacements.PORT = port;
 
     if (tailwind) {
-      fs.unlinkSync(path.join(name, '/src/index.css'))
+      fs.unlinkSync(path.normalize(`${name}/src/index.css`));
 
       await ncp(
-        path.join(__dirname, '../templates/application-extras/tailwind'),
+        path.join(__dirname, "../templates/application-extras/tailwind"),
         name
-      )
+      );
 
       const packageJSON = JSON.parse(
-        fs.readFileSync(path.join(name, 'package.json'), 'utf8')
-      )
-      packageJSON.devDependencies.tailwindcss = '^2.0.2'
+        fs.readFileSync(path.join(name, "package.json"), "utf8")
+      );
+      packageJSON.devDependencies.tailwindcss = "^2.0.2";
       fs.writeFileSync(
-        path.join(name, 'package.json'),
+        path.join(name, "package.json"),
         JSON.stringify(packageJSON, null, 2)
-      )
+      );
     }
   }
 
-  renameGitignore(name)
+  renameGitignore(name);
 
   glob.sync(`${name}/**/*`).forEach((file) => {
     if (fs.lstatSync(file).isFile()) {
-      templateFile(file, replacements)
+      templateFile(file, replacements);
     }
-  })
-}
+  });
+};
