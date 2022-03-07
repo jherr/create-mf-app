129bf8570e7cee6ef589e8ab44ef5ae050fea82e
Keith Robinson <kthrob@gmail.com>
2022-03-06 23:23:00 -0600

    Update react-application template, to work in turborepo.

--- templates/application/react/base/webpack.config.js
+++ templates/application/react/base/webpack.config.js
@@ -2,63 +2,66 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 const deps = require("./package.json").dependencies;
-module.exports = {
-  output: {
-    publicPath: "http://localhost:{{PORT}}/",
-  },
 
-  resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+const mf_config = {
+  name: "{{SAFE_NAME}}",
+  filename: "remoteEntry.js",
+  remotes: {},
+  exposes: {},
+  shared: {
+    ...deps,
+    react: {
+      singleton: true,
+      requiredVersion: deps.react,
+    },
+    "react-dom": {
+      singleton: true,
+      requiredVersion: deps["react-dom"],
+    },
   },
+};
 
-  devServer: {
-    port: {{PORT}},
-    historyApiFallback: true,
-  },
+module.exports = (env) => {
+  const prod = env.production;
 
-  module: {
-    rules: [
-      {
-        test: /\.m?js/,
-        type: "javascript/auto",
-        resolve: {
-          fullySpecified: false,
+  return {
+    output: {
+      publicPath: "http://localhost:{{PORT}}/",
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+    },
+    devServer: {
+      port: {{PORT}},
+      historyApiFallback: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.m?js/,
+          type: "javascript/auto",
+          resolve: {
+            fullySpecified: false,
+          },
         },
-      },
-      {
-        test: /\.(css|s[ac]ss)$/i,
-        use: ["style-loader", "css-loader", "postcss-loader"],
-      },
-      {
-        test: /\.(ts|tsx|js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
+        {
+          test: /\.(css|s[ac]ss)$/i,
+          use: ["style-loader", "css-loader", "postcss-loader"],
         },
-      },
+        {
+          test: /\.(ts|tsx|js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+      }),
+      ...(prod ? [new ModuleFederationPlugin(mf_config)] : [])
     ],
-  },
-
-  plugins: [
-    new ModuleFederationPlugin({
-      name: "{{SAFE_NAME}}",
-      filename: "remoteEntry.js",
-      remotes: {},
-      exposes: {},
-      shared: {
-        ...deps,
-        react: {
-          singleton: true,
-          requiredVersion: deps.react,
-        },
-        "react-dom": {
-          singleton: true,
-          requiredVersion: deps["react-dom"],
-        },
-      },
-    }),
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-    }),
-  ],
+  }
 };
