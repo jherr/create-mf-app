0ef1e52b0ea024a6745ffb82baa59cf8cad5755a
zmzlois <lois.z@comcord.vision>
2024-06-05 00:48:31 +0100

    Update create-mf-app.ts

--- bin/create-mf-app.ts
+++ bin/create-mf-app.ts
@@ -5,104 +5,104 @@ import fs from 'fs'
 import path from 'path'
 import { buildProject } from '../src/index'
 import { Project } from '../src/types'
-  ; (async function() {
-    const answers = await inquirer.prompt<Project>([
+; (async function () {
+  const answers = await inquirer.prompt<Project>([
+    {
+      type: 'input',
+      message: 'Pick the name of your app:',
+      name: 'name',
+      default: 'host'
+    },
+    {
+      type: 'list',
+      message: 'Project Type:',
+      name: 'type',
+      choices: ['Application', 'API Server', 'Library'],
+      default: 'Application'
+    }
+  ])
+
+  if (answers.type === 'Library') {
+    buildProject(answers)
+  }
+
+  if (answers.type === 'API Server') {
+    const templates = fs
+      .readdirSync(path.join(__dirname, '../templates/server'))
+      .sort()
+
+    const serverAnswers = await inquirer.prompt<Project>([
       {
         type: 'input',
-        message: 'Pick the name of your app:',
-        name: 'name',
-        default: 'host'
+        message: 'Port number:',
+        name: 'port',
+        default: '8080'
       },
       {
         type: 'list',
-        message: 'Project Type:',
-        name: 'type',
-        choices: ['Application', 'API Server', 'Library'],
-        default: 'Application'
+        message: 'Template:',
+        name: 'framework',
+        choices: templates,
+        default: 'express'
       }
     ])
 
-    if (answers.type === 'Library') {
-      buildProject(answers)
-    }
+    buildProject({
+      ...answers,
+      ...serverAnswers,
+      language: 'typescript'
+    })
+  }
 
-    if (answers.type === 'API Server') {
-      const templates = fs
-        .readdirSync(path.join(__dirname, '../templates/server'))
-        .sort()
+  if (answers.type === 'Application') {
+    const templates = fs
+      .readdirSync(path.join(__dirname, '../templates/application'))
+      .sort()
 
-      const serverAnswers = await inquirer.prompt<Project>([
-        {
-          type: 'input',
-          message: 'Port number:',
-          name: 'port',
-          default: '8080'
-        },
-        {
-          type: 'list',
-          message: 'Template:',
-          name: 'framework',
-          choices: templates,
-          default: 'express'
-        }
-      ])
+    const appAnswers = await inquirer.prompt<Project>([
+      {
+        type: 'input',
+        message: 'Port number:',
+        name: 'port',
+        default: '8080'
+      },
+      {
+        type: 'list',
+        message: 'Framework:',
+        name: 'framework',
+        choices: templates,
+        default: 'react'
+      },
+      {
+        type: 'list',
+        message: 'Language:',
+        name: 'language',
+        choices: ['typescript', 'javascript'],
+        default: 'javascript'
+      },
+      {
+        type: 'list',
+        message: 'CSS:',
+        name: 'css',
+        choices: ['CSS', 'Tailwind'],
+        default: 'CSS'
+      },
+      {
+        type: 'list',
+        message: 'Bundler:',
+        name: 'bundler',
+        choices: ['Webpack', 'Rspack'],
+        default: 'Webpack'
+      }
+    ])
 
-      buildProject({
-        ...answers,
-        ...serverAnswers,
-        language: 'typescript'
-      })
-    }
+    buildProject({
+      ...answers,
+      ...appAnswers
+    })
+  }
 
-    if (answers.type === 'Application') {
-      const templates = fs
-        .readdirSync(path.join(__dirname, '../templates/application'))
-        .sort()
-
-      const appAnswers = await inquirer.prompt<Project>([
-        {
-          type: 'input',
-          message: 'Port number:',
-          name: 'port',
-          default: '8080'
-        },
-        {
-          type: 'list',
-          message: 'Framework:',
-          name: 'framework',
-          choices: templates,
-          default: 'react'
-        },
-        {
-          type: 'list',
-          message: 'Language:',
-          name: 'language',
-          choices: ['typescript', 'javascript'],
-          default: 'javascript'
-        },
-        {
-          type: 'list',
-          message: 'CSS:',
-          name: 'css',
-          choices: ['CSS', 'Tailwind'],
-          default: 'CSS'
-        },
-        {
-          type: 'list',
-          message: 'Bundler:',
-          name: 'bundler',
-          choices: ['Webpack', 'Rspack'],
-          default: 'Webpack'
-        }
-      ])
-
-      buildProject({
-        ...answers,
-        ...appAnswers
-      })
-    }
-
-    shell.echo(`Your '${answers.name}' project is ready to go.
+  shell.echo(`Your '${answers.name}' project is ready to go.
 
 Next steps:
 
@@ -110,4 +110,4 @@ Next steps:
 ▶️ npm install
 ▶️ npm start
 `)
-  })()
+})()
