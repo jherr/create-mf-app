06d18f99d730ec42a29c9c3dae5c3da97b886d71
Cosme Valera <cosmevalerareales@gmail.com>
2024-06-27 18:36:48 +0200

    Show an informative message when compiling a rspack app

--- templates/application/preact/base/rspack.config.js
+++ templates/application/preact/base/rspack.config.js
@@ -1,6 +1,38 @@
 const rspack = require('@rspack/core')
 const refreshPlugin = require('@rspack/plugin-react-refresh')
 const isDev = process.env.NODE_ENV === 'development'
+const path = require('path');
+
+const printCompilationMessage = (status, port) => {
+  let messageColor, messageType, browserMessage
+
+  switch (status) {
+    case 'success':
+      messageColor = '\x1b[32m'
+      messageType = 'Compiled successfully!'
+      browserMessage = 'You can now view'
+      break
+    case 'failure':
+      messageColor = '\x1b[31m'
+      messageType = 'Compilation Failed!'
+      browserMessage = 'You can\'t now view'
+      break
+    case 'compiling':
+      messageColor = '\x1b[94m'
+      messageType = 'Compiling...'
+      browserMessage = 'Compiling the'
+      break
+  }
+
+  console.log(`\n\n
+${messageColor}${messageType}\x1b[0m\n
+${browserMessage} \x1b[1mRemote\x1b[0m in the browser.
+${messageColor}${messageType}\x1b[0m\n
+\x1b[1mLocal\x1b[0m:  http://localhost:\x1b[1m${port}\x1b[0m
+\x1b[1mLocal\x1b[0m:  http://localhost:\x1b[1m${port}\x1b[0m\n\n
+  `)
+}
+
 /**
  * @type {import('@rspack/cli').Configuration}
  */
@@ -14,10 +46,28 @@ module.exports = {
       disableTransformByDefault: true,
     },
   },
+  
   devServer: {
-    historyApiFallback: true,
     port: {{PORT}},
+    historyApiFallback: true,
+    watchFiles: [path.resolve(__dirname, 'src')],
+    onListening: function (devServer) {
+      const port = devServer.server.address().port
+
+      printCompilationMessage('compiling', port)
+
+      devServer.compiler.hooks.done.tap('OutputMessagePlugin', (stats) => {
+        setImmediate(() => {
+          if (stats.hasErrors()) {
+            printCompilationMessage('failure', port)
+          } else {
+            printCompilationMessage('success', port)
+          }
+        })
+      })
+    }
   },
+
   resolve: {
     extensions: ['.js','.jsx','.ts','.tsx','.json']
   },
