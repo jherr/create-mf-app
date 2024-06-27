588632d04bb16b4b4291b136dfaed1de70388bc7
Cosme Valera <cosmevalerareales@gmail.com>
2024-06-27 17:56:42 +0200

    Show an informative message when compiling a webpack app

--- templates/application/react-esm/base/webpack.config.js
+++ templates/application/react-esm/base/webpack.config.js
@@ -1,8 +1,40 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const path = require('path');
 const Dotenv = require('dotenv-webpack');
 
 const deps = require("./package.json").dependencies;
+
+const printCompilationMessage = (status, port) => {
+  let messageColor, messageType, browserMessage
+
+  switch (status) {
+    case 'success':
+      messageColor = '\x1b[32m'
+      messageType = 'Compiled successfully!'
+      browserMessage = 'You can now view'
+      break
+    case 'failure':
+      messageColor = '\x1b[31m'
+      messageType = 'Compilation Failed!'
+      browserMessage = 'You can\'t now view'
+      break
+    case 'compiling':
+      messageColor = '\x1b[94m'
+      messageType = 'Compiling...'
+      browserMessage = 'Compiling the'
+      break
+  }
+
+  console.log(`\n\n
+${messageColor}${messageType}\x1b[0m\n
+${browserMessage} \x1b[1mRemote\x1b[0m in the browser.
+${messageColor}${messageType}\x1b[0m\n
+\x1b[1mLocal\x1b[0m:  http://localhost:\x1b[1m${port}\x1b[0m
+\x1b[1mLocal\x1b[0m:  http://localhost:\x1b[1m${port}\x1b[0m\n\n
+  `)
+}
+
 module.exports = (_, argv) => ({
   output: {
     publicPath: "http://localhost:{{PORT}}/",
@@ -15,6 +47,22 @@ module.exports = (_, argv) => ({
   devServer: {
     port: {{PORT}},
     historyApiFallback: true,
+    watchFiles: [path.resolve(__dirname, 'src')],
+    onListening: function (devServer) {
+      const port = devServer.server.address().port
+
+      printCompilationMessage('compiling', port)
+
+      devServer.compiler.hooks.done.tap('OutputMessagePlugin', (stats) => {
+        setImmediate(() => {
+          if (stats.hasErrors()) {
+            printCompilationMessage('failure', port)
+          } else {
+            printCompilationMessage('success', port)
+          }
+        })
+      })
+    }
   },
 
   module: {
