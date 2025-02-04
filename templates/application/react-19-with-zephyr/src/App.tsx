8d0e49c526854a1be9afc617ff43ab6e6b2ac030
shane <shane@swalker.dev>
2025-02-04 07:38:42 -0600

    feat: added with-zephyr rspack example

---
+++ templates/application/react-19-with-zephyr/src/App.tsx
@@ -0,0 +1,14 @@
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+
+const App = () => (
+  <div className="{{CONTAINER}}">
+    <div>Name: {{ NAME }}</div>
+    <div>Framework: {{ FRAMEWORK }}</div>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("app") as HTMLElement);
+
+root.render(<App />);
